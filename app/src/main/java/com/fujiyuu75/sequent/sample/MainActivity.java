39b9f7996c5b8d02b98ecca7a81fc8777d4e1771
fujiyuu75 <fujiyuu75@gmail.com>
2017-05-25 16:52:19 +0900

    Add animation to find views

--- app/src/main/java/com/fujiyuu75/sequent/sample/MainActivity.java
+++ app/src/main/java/com/fujiyuu75/sequent/sample/MainActivity.java
@@ -3,6 +3,7 @@ package com.fujiyuu75.sequent.sample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
@@ -11,6 +12,8 @@ import android.widget.LinearLayout;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private int number = 0;
+    SparseArray<View> array = new SparseArray<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,19 +32,34 @@ public class MainActivity extends AppCompatActivity {
         ViewGroup rootView = (ViewGroup)contentView.getRootView();
         Log.d(TAG, String.format("%s %s %s %s", "rootView", rootView.getClass().getName(), "id", rootView.getId()));
 
-        int count = layout.getChildCount();
+        findChildLayouts(layout);
+    }
+
+    private void setAnimation(View v) {
+        AlphaAnimation anim = new AlphaAnimation(0, 1);
+        anim.setStartOffset(number * 50);
+        anim.setDuration(number * 500);
+        v.setAnimation(anim);
+
+        number++;
+    }
+
+    private void findChildLayouts(ViewGroup viewGroup) {
+        int count = viewGroup.getChildCount();
         Log.d(TAG, String.format("%s %s", "count", String.valueOf(count)));
 
-        for(int i = 0; i < count; ++i) {
-            View child = layout.getChildAt(i);
-
-            AlphaAnimation aa = new AlphaAnimation(0, 1);
-            aa.setStartOffset(i * 50);
-            aa.setDuration(i * 500);
-
-            child.setAnimation(aa);
-
-            Log.d(TAG, String.format("%s %s %s %s", "child", child.getClass().getName(), "id", String.valueOf(i * 1000)));
+        for (int i = 0; i < count; i++) {
+            View view = viewGroup.getChildAt(i);
+            if (view instanceof ViewGroup) {
+                Log.d(TAG, String.format("%s %s", "find", "viewGroup"));
+                findChildLayouts((ViewGroup) view);
+            } else {
+                Log.d(TAG, String.format("%s %s", "find", "view"));
+                setAnimation(view);
+                array.put(view.getId(), view);
+            }
+            Log.d(TAG, String.format("%s %s %s %s", "child", viewGroup.getClass().getName(), "id", String.valueOf(viewGroup.getId())));
         }
     }
+
 }
