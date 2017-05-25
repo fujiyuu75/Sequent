09c8456402ce517dda59d34a42f9a01ae5d00a1a
fujiyuu75 <fujiyuu75@gmail.com>
2017-05-25 17:22:40 +0900

    Delete unused method

--- app/src/main/java/com/fujiyuu75/sequent/sample/MainActivity.java
+++ app/src/main/java/com/fujiyuu75/sequent/sample/MainActivity.java
@@ -3,10 +3,6 @@ package com.fujiyuu75.sequent.sample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.util.SparseArray;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.AlphaAnimation;
 import android.widget.LinearLayout;
 
 import com.fujiyuu75.sequent.Sequent;
@@ -14,10 +10,6 @@ import com.fujiyuu75.sequent.Sequent;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private int number = 0;
-    private int startOffset = 50;
-    private int duration = 500;
-    SparseArray<View> array = new SparseArray<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,34 +20,5 @@ public class MainActivity extends AppCompatActivity {
         Sequent.start(layout);
 
         Log.d(TAG, String.format("%s %s %s %s", "layout", layout.getClass().getName(), "id", layout.getId()));
-
-//        findChildLayouts(layout);
-    }
-
-    private void setAnimation(View v) {
-        AlphaAnimation anim = new AlphaAnimation(0, 1);
-        anim.setStartOffset(number * startOffset);
-        anim.setDuration(number * duration);
-        v.setAnimation(anim);
-
-        number++;
-    }
-
-    private void findChildLayouts(ViewGroup viewGroup) {
-        int count = viewGroup.getChildCount();
-        Log.d(TAG, String.format("%s %s", "count", String.valueOf(count)));
-
-        for (int i = 0; i < count; i++) {
-            View view = viewGroup.getChildAt(i);
-            if (view instanceof ViewGroup) {
-                Log.d(TAG, String.format("%s %s", "find", "viewGroup"));
-                findChildLayouts((ViewGroup) view);
-            } else {
-                Log.d(TAG, String.format("%s %s", "find", "view"));
-                setAnimation(view);
-                array.put(view.getId(), view);
-            }
-            Log.d(TAG, String.format("%s %s %s %s", "child", viewGroup.getClass().getName(), "id", String.valueOf(viewGroup.getId())));
-        }
     }
 }
